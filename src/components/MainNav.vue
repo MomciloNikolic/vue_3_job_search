4e449c499944aaae96fe7fb645d724c6c5a391fb
Momcilo <momcilonikolicita@gmail.com>
2023-11-14 21:47:08 +0100

    V-FOR implemented to show nav trough array items

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -4,7 +4,26 @@
       <div
         class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8"
       >
-        <a class="flex h-full items-center text-xl" href="/">{{ company }}</a>
+        <!-- We can bind atributtes with v-bind or just : -->
+        <a
+          class="flex h-full items-center text-xl"
+          :href="url"
+          target="_blank"
+          >{{ company }}</a
+        >
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li
+              v-for="menuItem in menuItems"
+              :key="menuItem"
+              class="fis ml-9 h-full first:ml-0"
+            >
+              <a href="/#" class="flex h-full items-center py-2.5">{{
+                menuItem
+              }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -16,6 +35,15 @@ export default {
   data() {
     return {
       company: "MoMo Careers",
+      url: "https://careers.google.com",
+      menuItems: [
+        "Teams",
+        "Locations",
+        "Life at MoMo Careers",
+        "How we hire",
+        "Students",
+        "Jobs",
+      ],
     };
   },
 };
