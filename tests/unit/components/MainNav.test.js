626fded3e549c0ba7a9214ebc160e74e328b34dd
Momcilo <momcilonikolicita@gmail.com>
2023-11-14 20:55:53 +0100

    End of section 6

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -1,10 +1,12 @@
 import { render, screen } from "@testing-library/vue";
 
 import MainNav from "@/components/MainNav.vue";
+import { expect } from "vitest";
 
 describe("MainNav", () => {
   it("displays company name", () => {
     render(MainNav);
-    screen.getByText("MoMo Careers");
+    const companyName = screen.getByText("MoMo Careers");
+    expect(companyName).toBeInTheDocument();
   });
 });
