841cf4cadcecd950339b5b63050fdd0cbd44cee9
Momcilo <momcilonikolicita@gmail.com>
2023-11-19 23:35:51 +0100

    end of section 7

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -1,12 +1,31 @@
 import { render, screen } from "@testing-library/vue";
 
 import MainNav from "@/components/MainNav.vue";
-import { expect } from "vitest";
+import { expect, it } from "vitest";
 
 describe("MainNav", () => {
   it("displays company name", () => {
     render(MainNav);
     const companyName = screen.getByText("MoMo Careers");
     expect(companyName).toBeInTheDocument();
+  });
+
+  it("displays menu items for navigation", () => {
+    render(MainNav);
+    const navigationMenuItems = screen.getAllByRole("listitem");
+
+    const navigationMenuText = navigationMenuItems.map(
+      (item) => item.textContent
+    );
+    expect(navigationMenuText).toEqual([
+      "Teams",
+      "Locations",
+      "Life at MoMo Careers",
+      "How we hire",
+      "Students",
+      "Jobs",
+    ]);
+
+    console.log(navigationMenuText);
   });
 });
