e9bb6ed881526be468776a6d55eddb6a2b428aba
Momcilo <momcilonikolicita@gmail.com>
2023-11-29 23:28:48 +0100

    Added font awesome icons paca and Vue dependencies, added The SubNav

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -35,6 +35,7 @@
           />
         </div>
       </div>
+      <the-subnav />
     </div>
   </header>
 </template>
@@ -42,12 +43,14 @@
 <script>
 import ActionButton from "@/components/ActionButton.vue";
 import ProfileImage from "@/components/ProfileImage.vue";
+import TheSubnav from "@/components/TheSubnav.vue";
 
 export default {
   name: "MainNav",
   components: {
     ActionButton,
     ProfileImage,
+    TheSubnav,
   },
   data() {
     return {
